d97e9f0d9bc48b74af9bd59b089f9795c1f5c527
yash musanwar <yashmusanwar123@gmail.com>
2026-09-12 19:23:50 +0530

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-gray-600 text-white text-center p-3 font-semibold">
-      © 2025 Created By yash Musanwar...
+      © 2025 Created By Yash Musanwar...
     </footer>
   );
 };
